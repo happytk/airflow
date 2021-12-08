be9c3363a08396d13b1c63a643c12b7d5d6e4df3
Ubuntu <ubuntu@ip-172-31-43-24.ap-northeast-2.compute.internal>
2021-12-08 15:00:01 +0000

    testd

---
+++ tests/dags/testd.py
@@ -0,0 +1,13 @@
+import json
+from datetime import datetime
+
+from airflow.decorators import dag, task
+@dag(schedule_interval=None, start_date=datetime(2021, 1, 1), catchup=False, tags=['example'])
+def test_docker():
+    @task.docker(image='smizy/scikit-learn', multiple_outputs=True)
+    def load(order_data_dict: dict):
+        from sklearn.datasets import load_iris
+        data = load_iris()
+        return data.target_names
+
+testd = test_docker()
