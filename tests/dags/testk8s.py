610e4aa4d3690deadfd839a1c9d52de256b4586b
Ubuntu <ubuntu@ip-172-31-43-24.ap-northeast-2.compute.internal>
2021-12-08 16:05:10 +0000

    k8s

--- tests/dags/testk8s.py
+++ tests/dags/testk8s.py
@@ -67,4 +67,22 @@ run = KubernetesPodOperator(
     dag=dag,
 )
 
+write_xcom = KubernetesPodOperator(
+    namespace='default',
+    image='alpine',
+    cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '{{ dag_run.conf.get('message', '[1,2,3,4]') if dag_run else '[1,2,3,4]' }}' > /airflow/xcom/return.json"],
+    name="write-xcom",
+    do_xcom_push=True,
+    is_delete_operator_pod=True,
+    in_cluster=True,
+    task_id="write-xcom",
+    get_logs=True,
+)
+
+pod_task_xcom_result = BashOperator(
+    bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
+    task_id="pod_task_xcom_result",
+)
+
 start >> run
+start >> write_xcom >> pod_task_xcom_result
